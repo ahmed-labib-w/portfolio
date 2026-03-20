a2d655051f98b845e409349bd18d7455d86888b7
budderrar <ahmednasser82380@gmail.com>
2026-03-20 21:05:26 +0200

    edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,53 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div id='home' className='px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32'>
-      <div className='flex flex-col items-center justify-center gap-10 text-white'>
-        <div className='flex max-w-[600px] flex-col items-center justify-center gap-3 text-center'>
-          <motion.div initial={{y:-50, opacity:0}} animate={{y:0, opacity:1}} transition={{duration: 0.8, delay:0}}><h1 className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-6xl font-semibold transition-all duration-300 hover:-translate-y-1 hover:text-7xl cursor-default'>Ahmed Nasser</h1></motion.div>
-          <motion.div initial={{y:50, opacity:0}} animate={{y:0, opacity:1}} transition={{duration: 0.8, delay:0}}>
-          <h3 className='bg-gradient-to-r from-pink-500 to-blue-500 bg-clip-text text-transparent text-2xl md:text-3xl'>Web Developer</h3>
-          <p className='md:text-base text-pretty text-sm text-gray-400'>A Computer science graduate with a passion for web development, based in <em className='text-bold text-base'>Cairo, Egypt</em></p>  
+    <div
+      id="home"
+      className="px-8 flex min-h-screen w-full items-center justify-center py-20 md:px-32"
+    >
+      <div className="flex flex-col items-center justify-center gap-6 text-white">
+        {/* pfp */}
+        <motion.img
+          src="/public/avatar.png"
+          alt="Ahmed Nasser"
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="w-32 h-32 rounded-full object-cover border-4 border-purple-500 shadow-lg shadow-purple-500/30"
+        />
+
+        <div className="flex max-w-[600px] flex-col items-center justify-center gap-3 text-center">
+          <motion.h1
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.1 }}
+            className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent text-5xl md:text-6xl font-semibold transition-all duration-300 hover:-translate-y-1 cursor-default"
+          >
+            Ahmed Nasser
+          </motion.h1>
+
+          <motion.div
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+            className="flex flex-col items-center gap-3"
+          >
+            <h3 className="bg-gradient-to-r from-pink-500 to-blue-500 bg-clip-text text-transparent text-2xl md:text-3xl">
+              Software Developer
+            </h3>
+            <p className="text-sm md:text-base text-gray-400 text-pretty">
+              Hi, I'm Ahmed — a Computer Science graduate exploring software
+              development across modern web technologies, with hands-on
+              experience in React and API-driven applications. applications with
+              React & Next.js. Based In{" "}
+              <em className="font-bold text-white">Giza, Egypt</em>
+            </p>
           </motion.div>
         </div>
       </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,81 +1,126 @@
-import { motion } from 'framer-motion'
-import React from 'react'
-import image1 from '../assets/iphone-clone.png'
-import image2 from '../assets/ecommerce.png'
-import image3 from '../assets/academic-advisor.png'
-
+import { motion } from "framer-motion";
+import image1 from "../assets/iphone-landing page.png";
+import image2 from "../assets/ecommerce.png";
+import image3 from "../assets/academic-advisor.png";
+import image4 from "../assets/movie-app.png";
+import image5 from "../assets/movie-listing.png";
 
 const projectsData = [
   {
-    image: image1,
-    title: "Apple Iphone15 website clone",
-    description: "Successfuly cloned the website for the Iphone 15, all while teaching myself how big companies like Apple design their websites for the best user experience",
-    technologies: [`ReactJS`, "TailwindCSS", "ThreeJS", "GSAP Animations", "Vite"],
-    githubLink: "https://github.com/budderrar/apple_clone"
+    image: image4,
+    title: "Movie Searching App",
+    description:
+      "A movie discovery web application built with React that allows users to browse trending movies, and manage a personal favorites list.",
+    technologies: [
+      "React",
+      "React Router",
+      "Vanilla CSS",
+      "Context API",
+      "TMDB API",
+    ],
+    githubLink: "https://github.com/ahmed-labib-w/Movie-App",
+  },
+  {
+    image: image5,
+    title: "Job Listing App",
+    description:
+      "A Single Page CRUD Application built with React that allows users to create, edit, and delete job listings.",
+    technologies: ["React", "React Router", "TailwindCSS"],
+    githubLink: "https://github.com/ahmed-labib-w/job-listing",
   },
   {
     image: image2,
     title: "Ecommerce website",
-    description: "This is an Ecommerce website that i developed and maintained using NextJS in which i've learnt how to work with REST APIs and managing the state of the data using CONTEXT API",
-    technologies: ["NextJS", "TailwindCSS", "Context API", "ESlint"],
-    githubLink: "https://github.com/budderrar/Ecommerce_site"
+    description:
+      "This is an Ecommerce website i developed as my final project for my frontend training/diploma at Route Academy that displays various products fetched from a REST API",
+    technologies: ["Next", "TailwindCSS", "Context API", "FORMIK", "POSTMAN"],
+    githubLink: "https://github.com/budderrar/Ecommerce_site",
+  },
+  {
+    image: image1,
+    title: "Apple Iphone15 Landing Page",
+    description:
+      "An iphone landing page inspired by Apple's IPHONE 15 landing page",
+    technologies: [`ReactJS`, "TailwindCSS", "ThreeJS", "GSAP Animations"],
+    githubLink: "https://github.com/budderrar/apple_clone",
   },
   {
     image: image3,
     title: "Academic Advisor",
-    description: "This was my graduation project in which i've heavily contributed to the backend logic aswell as assisting withthe frontend development",
-    technologies: ["NodeJS", "ExpressJS", "MONGODB", "HTML CSS JS", "Bootstrap"],
-    githubLink: "https://github.com/budderrar/Academic-Advisor"
-  }
-]
-
+    description: "Graduation Project",
+    technologies: [
+      "NodeJS",
+      "ExpressJS",
+      "MONGODB",
+      "HTML CSS JS",
+      "Bootstrap",
+    ],
+    githubLink: "https://github.com/budderrar/Academic-Advisor",
+  },
+];
 
 const ProjectCard = ({ project }) => {
   const handleRedirect = () => {
     window.open(project.githubLink, "_blank");
-  }
+  };
   return (
-    
-    <motion.div initial={{y:100, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 0.8, delay:0}} className='flex flex-col items-center gap-8 md:flex-row md:gap-24'>
-      <img src={project.image} alt="" className='w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[300px]' onClick={handleRedirect}/>
+    <motion.div
+      initial={{ y: 100, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.8, delay: 0 }}
+      className="flex flex-col items-center gap-8 md:flex-row md:gap-24"
+    >
+      <img
+        src={project.image}
+        alt=""
+        className="w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[300px]"
+        onClick={handleRedirect}
+      />
 
-      <div className='flex flex-col gap-5'>
-        <div className='flex flex-col gap-3'>
-          <div className='text-xl font-semibold'>{project.title}</div>
-          <p className='text-gray-400'>{project.description}</p>
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-3">
+          <div className="text-xl font-semibold">{project.title}</div>
+          <p className="text-gray-400">{project.description}</p>
         </div>
 
-        <div className='flex flex-wrap gap-5'>
+        <div className="flex flex-wrap gap-5">
           {project.technologies.map((tech, index) => (
-            <span key={index} className='rounded-lg bg-black p-3'>
+            <span key={index} className="rounded-lg bg-black p-3">
               {tech}
             </span>
           ))}
         </div>
 
-        <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] w-80 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50" onClick={handleRedirect}>
-        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-        Check out the repository on github 😁🔗
-        </span>
-      </button>
+        <button
+          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] w-80 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          onClick={handleRedirect}
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            Check out the repository on github 😁🔗
+          </span>
+        </button>
       </div>
     </motion.div>
-   
   );
 };
 
-
-
 const Projects = () => {
   return (
-    <div id='projects' className='flex min-h-screen w-full flex-col items-center justify-center gap-16 p-4 md:px-14 md:py-24'>
-      
-      <motion.h1 
-      initial={{y:100, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 0.8, delay:0}}
-      className='text-4xl font-light text-white md:text-6xl'>My Projects</motion.h1>
-     
-      <div className='flex w-full max-w-[1000px] flex-col gap-16 text-white'>
+    <div
+      id="projects"
+      className="flex min-h-screen w-full flex-col items-center justify-center gap-16 p-4 md:px-14 md:py-24"
+    >
+      <motion.h1
+        initial={{ y: 100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0 }}
+        className="text-4xl font-light text-white md:text-6xl"
+      >
+        My Projects
+      </motion.h1>
+
+      <div className="flex w-full max-w-[1000px] flex-col gap-16 text-white">
         {projectsData.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
